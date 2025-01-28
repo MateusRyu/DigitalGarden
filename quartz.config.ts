872033b66d5fb3a73d6d1b8f01d1a75ca2732e30
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2025-01-28 00:39:42 -0300

    build: Replace placeholder config with the real data

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Ryu",
+    pageTitleSuffix: " - Digital Garden!",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "pt-BR",
+    baseUrl: "ryu.blog.br",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
